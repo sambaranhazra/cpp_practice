7ef9534696433bb17e257a7c267159571ff6518a
sambaran <sambaran2010@gmail.com>
2014-06-19 23:35:13 +0530

    C++ First Project

---
+++ SampleCPP/src/SampleCPP.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : SampleCPP.cpp
+// Author      : Sambaran Hazra
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	string name;
+	cout<<"Enter your name:"<<endl;
+	getline(cin,name);
+	cout<<"Hello "<<name<<endl;
+	return 0;
+}
